76979b0a776b6d01cb00bcec9425855f702ba413
Serhii Mozhaiskyi <serhii.mozhaiskyi@globallogic.com>
2016-12-16 09:55:51 +0200

    update sample app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('ES6 app');
+import $ from 'jquery';
+
+import './style.css';
+
+class HelloWorld {
+    constructor(data) {
+        this.prop1 = data;
+    }
+
+    line1() {
+        this.constructor.append(this.prop1.join(';'));
+    }
+
+    static append(string) {
+        $('#output').text(string);
+    }
+}
+
+const hello = new HelloWorld(['test', 'data']);
+
+hello.line1();
