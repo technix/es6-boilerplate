1b19b6755a5ac8c306bd6deb2be8c792b37f1dac
techniX <sergei.mozhaisky@gmail.com>
2017-02-21 20:03:23 +0200

    Update packages; migrate to webpack2

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -18,19 +18,19 @@ export default {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel?presets[]=es2015'
+                loader: 'babel-loader?presets[]=es2015'
             },
             {
                 test: /\.css/,
-                loader: ExtractTextPlugin.extract('style', 'css')
+                loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})
             },
             {
                 test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file'
+                loader: 'file-loader'
             },
             {
                 test: /\.html$/,
-                loader: 'raw'
+                loader: 'raw-loader'
             }
         ]
     },
@@ -40,14 +40,14 @@ export default {
             inject: 'body'
         }),
         new ExtractTextPlugin('styles.css'),
-        new webpack.optimize.CommonsChunkPlugin(
-            /* chunkName= */ 'vendor',
-            /* filename= */ 'vendor.js',
-            (module) =>
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.js',
+            minChunks: (module) =>
                 module.resource &&
                     module.resource.indexOf('.js') !== -1 &&
                     module.resource.indexOf(path.join(__dirname, 'node_modules')) !== -1
-        )
+        })
     ],
     devServer: {
         contentBase: './src/assets',
